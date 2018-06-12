20539ba2ba37f198bedcab22e6dc6e587e8939c2
ushachev <vit.ush@mail.ru>
2018-06-12 09:27:04 +0300

    Первая неделя завершена

--- postcss.config.js
+++ postcss.config.js
@@ -11,12 +11,13 @@ module.exports = {
     require("postcss-advanced-variables")({
       variables: require("./src/assets/styles/variables")
     }),
+    require("math-calc"),
     require("postcss-nested"),
     require("postcss-rgb"),
     require("postcss-inline-comment"),
     require("postcss-inline-svg")({
       removeFill: true,
-      path: "./src/assets/images/icons"
+      path: "./src/assets/images/bg-svg"
     }),
     require("postcss-svgo"),
     require("cssnano")()
