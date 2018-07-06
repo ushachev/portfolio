aca974775d3983eee8aa0095008a755aad9753ff
ushachev <vit.ush@mail.ru>
2018-07-06 20:03:11 +0300

    selenium test and feedback form validate add

--- src/assets/scripts/modules/feedback.js
+++ src/assets/scripts/modules/feedback.js
@@ -5,23 +5,67 @@ new Vue({
   el: "#review-form",
   data() {
     return {
+      showModal: false,
+      modalMessage: "",
       parameters: {
         name: "",
-        email: ""
+        email: "",
+        feed: ""
       }
     };
   },
   methods: {
+    addErrorClass(elem) {
+      elem.classList.add("invalid");
+      elem.nextSibling.classList.add("tooltip_active");
+    },
+    deleteErrorClass(e) {
+      e.target.classList.remove("invalid");
+      e.target.nextSibling.classList.remove("tooltip_active");
+    },
+    showModalBlock() {
+      this.showModal = true;
+      document.body.classList.add("scroll-off");
+    },
+    closeModal() {
+      this.showModal = false;
+      document.body.classList.remove("scroll-off");
+    },
+    checkForm() {
+      if (
+        this.parameters.name &&
+        this.parameters.email &&
+        this.parameters.feed
+      ) {
+        if (!this.validEmail(this.parameters.email)) {
+          const emailInput = document.getElementById("mail");
+          this.addErrorClass(emailInput);
+          return false;
+        }
+        return true;
+      }
+      this.modalMessage = "Не все поля заполнены";
+      this.showModalBlock();
+      return false;
+    },
     submitForm() {
-      let url = "https://jsonplaceholder.typicode.com/posts";
-      axios
-        .post(url, this.parameters)
-        .then(response => {
-          console.log(JSON.stringify(response, null, 2));
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      const url = "https://jsonplaceholder.typicode.com/posts";
+      if (this.checkForm()) {
+        axios
+          .post(url, this.parameters)
+          .then(response => {
+            console.log(JSON.stringify(response, null, 2));
+            this.modalMessage = "Ваше сообщение отправлено";
+            this.showModalBlock();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+    },
+    validEmail(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
     }
   }
 });
